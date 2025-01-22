4427085220bd1c6f83971cd678b8fb037d989660
Abdel Rahman Jebril <abdjibrel@gmail.com>
2025-01-22 15:35:47 +0300

    Initial commit for Library Management System

--- src/main/java/com/library/management/dao/GenericDAO.java
+++ src/main/java/com/library/management/dao/GenericDAO.java
@@ -2,6 +2,18 @@ package com.library.management.dao;
 
 import java.util.List;
 
+/**
+ * A generic Data Access Object (DAO) interface to provide common CRUD operations.
+ *
+ * <p>
+ * This interface is designed to be reusable for different entity types
+ * (e.g., User, Book, Transaction) by utilizing Java Generics. It defines
+ * the core database operations that are common across all types of entities,
+ * ensuring consistency and reducing code duplication.
+ * </p>
+ *
+ * @param <T> the type of the entity that this DAO manages (e.g., User, Book, Transaction).
+ */
 public interface GenericDAO<T> {
     void add(T entity);
     void update(T entity);
